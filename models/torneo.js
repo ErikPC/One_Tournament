c7cac4e98854111bd349efc817cade4f19dc9275
ErikPC <epardillo@cifpfbmoll.eu>
2023-05-08 17:40:37 +0200

    feat: añadir modelo torneo

---
+++ models/torneo.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const torneoSchema = new Schema({
+  nombreTorneo: { type: String, required: true },
+  nombreTienda: { type: String, required: true },
+  fecha: { type: Date, required: true },
+  numeroParticipantes: { type: Number, required: true },
+});
+
+const Torneo = mongoose.model("Torneo", torneoSchema);
+
+module.exports = Torneo;
