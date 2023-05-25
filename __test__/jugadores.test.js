5f83fb05c7dd5971dbeeedf13ffcf58d02158a4d
ErikPC <epardillo@cifpfbmoll.eu>
2023-05-25 23:41:54 +0200

    test: crear test jugadores

---
+++ __test__/jugadores.test.js
@@ -0,0 +1,97 @@
+const request = require("supertest");
+const app = require("../app");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const Jugador = require("../models/jugador");
+const jugadorPost = require("../db/jugadores/jugadorPost.json");
+const jugadorUpdate = require("../db/jugadores/jugadorUpdate.json");
+const User = require("../models/user");
+const userPost = require("../db/user/userPost.json");
+
+const uri = process.env.MONGO_URI_TEST;
+jest.setTimeout(10000);
+
+let token = "";
+beforeAll(async () => {
+  mongoose.set("strictQuery", false);
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await request(app).post("/api/register").send(userPost);
+  const response = await request(app).post("/api/login").send(userPost);
+  token = response.body.token;
+});
+afterAll(async () => {
+  await Jugador.deleteMany({});
+  await User.deleteMany({});
+  await mongoose.connection.close();
+});
+
+describe("test jugador", () => {
+  test("register jugador", async () => {
+    const response = await request(app)
+      .post("/api/jugador")
+      .set("Authorization", `${token}`)
+      .send(jugadorPost);
+    expect(response.statusCode).toBe(201);
+  });
+
+  test("register jugador fail without token", async () => {
+    const response = await request(app).post("/api/jugador").send(jugadorPost);
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("get jugador", async () => {
+    const response = await request(app)
+      .get("/api/jugador")
+      .set("Authorization", `${token}`);
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("get jugador fail without token", async () => {
+    const response = await request(app).get("/api/jugador");
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("get jugador by name", async () => {
+    const response = await request(app)
+      .get("/api/jugador/Falopio")
+      .set("Authorization", `${token}`);
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("get jugador by name fail without token", async () => {
+    const response = await request(app).get("/api/jugador/Pepe");
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("update jugador", async () => {
+    const response = await request(app)
+      .put("/api/jugador/Falopio")
+      .set("Authorization", `${token}`)
+      .send(jugadorUpdate);
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("update jugador fail without token", async () => {
+    const response = await request(app)
+      .put("/api/jugador/Pepe")
+      .send(jugadorUpdate);
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("delete jugador", async () => {
+    const response = await request(app)
+      .delete("/api/jugador/Falopio")
+      .set("Authorization", `${token}`);
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("delete jugador fail without token", async () => {
+    const response = await request(app).delete("/api/jugador/Pepe");
+    expect(response.statusCode).toBe(403);
+  });
+});
