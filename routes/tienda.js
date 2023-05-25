b7934ac219e0c8cb66e0deb50e4d68f194f8deaf
ErikPC <epardillo@cifpfbmoll.eu>
2023-05-25 23:10:13 +0200

    feat: añadir autenticacion a las rutas de tienda

--- routes/tienda.js
+++ routes/tienda.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const TiendaController = require("../controllers/tienda");
+const md_auth = require("../middleware/autenticar");
 
 const api = express.Router();
 
-api.get("/tienda", TiendaController.getTiendas);
-api.post("/tienda", TiendaController.createTienda);
-api.get("/tienda/:nombre", TiendaController.getTienda);
-api.delete("/tienda/:nombre", TiendaController.deleteTienda);
-api.put("/tienda/:nombre", TiendaController.updateTienda);
+api.get("/tienda", [md_auth.ensureAuth], TiendaController.getTiendas);
+api.post("/tienda", [md_auth.ensureAuth], TiendaController.createTienda);
+api.get("/tienda/:nombre", [md_auth.ensureAuth], TiendaController.getTienda);
+api.delete(
+  "/tienda/:nombre",
+  [md_auth.ensureAuth],
+  TiendaController.deleteTienda
+);
+api.put("/tienda/:nombre", [md_auth.ensureAuth], TiendaController.updateTienda);
 
 module.exports = api;
