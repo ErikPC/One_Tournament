84f118aa5bbf114fdf0306c84bc06f655c9d62b3
ErikPC <epardillo@cifpfbmoll.eu>
2023-05-25 23:31:27 +0200

    feat: proteger las rutas

--- routes/jugador.js
+++ routes/jugador.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const JugadorController = require("../controllers/jugador");
+const md_auth = require("../middleware/autenticar");
 
 const api = express.Router();
 
-api.post("/jugador", JugadorController.createJugador);
-api.get("/jugador", JugadorController.getJugadores);
-api.get("/jugador/:nombre", JugadorController.getJugador);
-api.delete("/jugador/:nombre", JugadorController.deleteJugador);
-api.put("/jugador/:nombre", JugadorController.updateJugador);
-api.get("/jugador/:nombre/puntosTorneo", JugadorController.getPuntosTorneo);
-api.put("/jugador/:nombre/:resultado", JugadorController.setResultado);
+api.post("/jugador", [md_auth.ensureAuth], JugadorController.createJugador);
+api.get("/jugador", [md_auth.ensureAuth], JugadorController.getJugadores);
+api.get("/jugador/:nombre", [md_auth.ensureAuth], JugadorController.getJugador);
+api.delete(
+  "/jugador/:nombre",
+  [md_auth.ensureAuth],
+  JugadorController.deleteJugador
+);
+api.put(
+  "/jugador/:nombre",
+  [md_auth.ensureAuth],
+  JugadorController.updateJugador
+);
+api.get(
+  "/jugador/:nombre/puntosTorneo",
+  [md_auth.ensureAuth],
+  JugadorController.getPuntosTorneo
+);
+api.put(
+  "/jugador/:nombre/:resultado",
+  [md_auth.ensureAuth],
+  JugadorController.setResultado
+);
 module.exports = api;
