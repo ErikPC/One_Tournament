19c7bcf382618df4b590513566626e40f18a830c
ErikPC <epardillo@cifpfbmoll.eu>
2023-05-29 09:46:20 +0200

    feat: proteger las rutas

--- routes/torneo.js
+++ routes/torneo.js
@@ -1,35 +1,55 @@
 const express = require("express");
 const TorneoController = require("../controllers/torneo");
 
+const md_auth = require("../middleware/autenticar");
+
 const api = express.Router();
 
-api.post("/torneo", TorneoController.createTorneo);
-api.get("/torneo", TorneoController.getTorneos);
-api.get("/torneo/:fecha/:nombreTienda", TorneoController.getTorneo);
-api.delete("/torneo/:fecha/:nombreTienda", TorneoController.deleteTorneo);
-api.put("/torneo/:fecha/:nombreTienda", TorneoController.updateTorneo);
+api.post("/torneo", [md_auth.ensureAuth], TorneoController.createTorneo);
+api.get("/torneo", [md_auth.ensureAuth], TorneoController.getTorneos);
+api.get(
+  "/torneo/:fecha/:nombreTienda",
+  [md_auth.ensureAuth],
+  TorneoController.getTorneo
+);
+api.delete(
+  "/torneo/:fecha/:nombreTienda",
+  [md_auth.ensureAuth],
+  TorneoController.deleteTorneo
+);
+api.put(
+  "/torneo/:fecha/:nombreTienda",
+  [md_auth.ensureAuth],
+  TorneoController.updateTorneo
+);
 api.put(
   "/torneo/:fecha/:nombreTienda/anadir/:jugador",
+  [md_auth.ensureAuth],
   TorneoController.añadirParticipante
 );
 api.put(
   "/torneo/:fecha/:nombreTienda/eliminar/:jugador",
+  [md_auth.ensureAuth],
   TorneoController.eliminarParticipante
 );
 api.get(
   "/torneo/:fecha/:nombreTienda/jugadores",
+  [md_auth.ensureAuth],
   TorneoController.getPuntosJugadores
 );
 api.put(
   "/torneo/:fecha/:nombreTienda/calcular/calculoRonda",
+  [md_auth.ensureAuth],
   TorneoController.calculoRonda
 );
 api.get(
   "/torneo/:fecha/:nombreTienda/emparejar",
+  [md_auth.ensureAuth],
   TorneoController.emparejarTorneo
 );
 api.put(
   "/torneo/:fecha/:nombreTienda/pairing/:jugador1/:jugador2/",
+  [md_auth.ensureAuth],
   TorneoController.pairing
 );
 module.exports = api;
