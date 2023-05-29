13d0ea6341f05fbc7219f82ef7c143df6a6ffe0a
ErikPC <epardillo@cifpfbmoll.eu>
2023-05-29 11:09:34 +0200

    refactor: eliminar funcion repository no usada

--- repository/repositoryTorneo.js
+++ repository/repositoryTorneo.js
@@ -25,17 +25,10 @@ function getTorneo(fecha, nombreTienda) {
   return Torneo.findOne({ fecha: fecha, nombreTienda: nombreTienda });
 }
 
-function getListaJugadores(fecha, nombreTienda) {
-  return Torneo.findOne({ fecha: fecha, nombreTienda: nombreTienda }).select(
-    "jugadores"
-  );
-}
-
 module.exports = {
   createTorneo,
   getTorneos,
   deleteTorneo,
   updateTorneo,
   getTorneo,
-  getListaJugadores,
 };
